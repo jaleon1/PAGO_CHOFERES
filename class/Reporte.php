4a9b4d4405c432ed54341b56fc69d3b55185f2f7
Car11 <carlos.echc11@gmail.com>
2018-02-16 21:55:42 -0600

    commit

--- class/Reporte.php
+++ class/Reporte.php
@@ -13,6 +13,9 @@ if(isset($_POST["action"])){
         case "ConsultaFiltro":
             echo json_encode($reporte->ConsultaFiltro($_POST["idfiltro"], $_POST["tipo"]));
             break;
+        case "ConsultaFiltroFecha":
+            echo json_encode($reporte->ConsultaFiltro($_POST["idfiltro"], $_POST["tipo"], $_POST["filtrofecha"] ));
+            break;
     }
 }
 
@@ -40,25 +43,46 @@ class Reporte
         }
     }
 
-    function ConsultaFiltro($idfiltro, $tipo){
+    function ConsultaFiltro($idfiltro=NULL, $tipo=NULL, $filtrofecha=NULL){
         try {
+            // Construye el where
+            date_default_timezone_set("America/Costa_Rica");
+            $where= '';
             switch($tipo){
                 case 'chofer':
-                    $tablafiltro = 'c';
+                    $where = ' c.id= :idfiltro ';
                     break;
                 case 'naviera':
-                    $tablafiltro = 'nav';
+                    $where = ' nav.id= :idfiltro ';
                     break;
                 case 'finca':
-                    $tablafiltro = 'fin';
+                    $where = ' fin.id= :idfiltro ';
                     break;
             }
+            switch($filtrofecha){
+                case 'Diario':
+                    $inidate = new DateTime();
+                    $inidate->sub(new DateInterval('P1D'));
+
+                    $where += ' f.fecha between  $inidate and $final ';
+                    break;
+                case 'Mensual':
+                    $where += ' ';
+                    break;
+                case 'Semanal':
+                    $where += '  ';
+                    break;
+                case 'Anual':
+                    $where= '';
+                    break;
+            }
+            //
             $sql = "SELECT f.id, f.comprobante, c.nombre as chofer,f.fecha, f.contenedor, f.placa, fin.nombre as finca, nav.nombre as naviera, kms, valorkm, totalpago
                 FROM formulariopago f inner join calculokm cal on cal.id=f.idcalculokm
                     inner join finca fin on fin.id=cal.idfinca
                     inner join naviera nav on nav.id=cal.idnaviera
                     inner join chofer c on c.id=f.idchofer
-                WHERE $tablafiltro.id= :idfiltro
+                WHERE $where
                 ORDER BY comprobante DESC";
              $param= array(':idfiltro'=>$idfiltro);
              $data= DATA::Ejecutar($sql, $param);
